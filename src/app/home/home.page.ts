45725d8f9ccbc2717d6a3c2a7e770d09535600b5
Mandi Li <boomsaka@udel.edu>
2022-06-15 10:15:29 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {createWorker} from 'tesseract.js';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  worker: Tesseract.Worker;
+  workerReady = false;
+  image = './assets/eng_bw.png'
+  captureProgress = 0;
+  ocrResult = '';
+  statusMsg = '';
 
-  constructor() {}
+  constructor() {
+    this.loadWorker();
+  }
 
+  async loadWorker(){
+    this.worker = createWorker({
+      logger: progress => {
+        console.log(progress);
+        this.statusMsg = progress.status;
+        if (progress.status == 'recognizing text') {
+          this.captureProgress = parseInt('' + progress.progress * 100);
+        }
+      }
+    });
+
+    await this.worker.load();
+    await this.worker.loadLanguage('eng');
+    await this.worker.initialize('eng');
+    console.log('FIN');
+    this.workerReady = true;
+  }
+
+  async captureImage(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    console.log('image: ', image);
+    this.image = image.dataUrl;
+  }
+
+  async recongnizeImage(){
+    const result = await this.worker.recognize(this.image);
+    console.log(result);
+    this.ocrResult = result.data.text;
+  }
 }
